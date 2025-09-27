668aacd2079e1f1934bc35d4767c581edbb488e9
matedulcent <mate.denti@gmail.com>
2025-09-26 22:04:04 -0300

    id de idex

--- app/index.tsx
+++ app/index.tsx
@@ -5,12 +5,20 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 export default function HomeScreen() {
   const router = useRouter();
 
+  const handleElegirSucursal = () => {
+    // Generar un ID de cliente aleatorio
+    const clientId = Math.floor(100000 + Math.random() * 900000).toString();
+
+    // Navegar a la pantalla de selección de sucursal pasando el clientId
+    router.push({
+      pathname: "./screens/Seleccion_Sucursal",
+      params: { clientId },
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Pressable
-        style={styles.button}
-        onPress={() => router.push("./screens/Seleccion_Sucursal")}
-      >
+      <Pressable style={styles.button} onPress={handleElegirSucursal}>
         <Text style={styles.buttonText}>Elegir Sucursal</Text>
       </Pressable>
 
