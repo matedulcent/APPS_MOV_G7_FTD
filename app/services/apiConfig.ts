eb43ad3556e0f2cc339ccef53ec11329f498fb03
matedulcent <mate.denti@gmail.com>
2025-11-11 19:02:25 -0300

    Update apiConfig.ts

--- app/services/apiConfig.ts
+++ app/services/apiConfig.ts
@@ -21,7 +21,7 @@ import { Platform } from "react-native";
 
 //const LOCAL_IP = "192.168.0.158"; //Tobio
 //const LOCAL_IP = "172.29.159.108"; //Santi
-const LOCAL_IP = "192.168.68.118"; //Mateo
+const LOCAL_IP = "172.29.151.156"; //Mateo
 //                    ▲
 //                   / \
 //                  /   \
